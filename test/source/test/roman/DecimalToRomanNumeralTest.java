52c5cfa2daf78fc12bb51467a93dd4a312368d11
Niccolò <rossinikolai@gmail.com>
2019-11-07 11:00:47 +0100

    first test

--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -1,5 +1,18 @@
 package test.roman;
 
+import dssc.roman.RomanNumeral;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class DecimalToRomanNumeralTest {
+
+    // first test: verifying that 0 returns empty string
+    @Test
+    void zeroIsEmptyString() {
+        RomanNumeral romanNumeral = new RomanNumeral(0);
+        assertEquals("", romanNumeral.toString());
+    }
 
 }
