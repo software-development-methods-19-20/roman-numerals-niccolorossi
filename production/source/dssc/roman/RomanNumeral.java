798eb32e33d1b1b6fd952326480e1427202adc21
Niccolò <rossinikolai@gmail.com>
2019-11-12 09:44:45 +0100

    second test

--- production/source/dssc/roman/RomanNumeral.java
+++ production/source/dssc/roman/RomanNumeral.java
@@ -1,11 +1,18 @@
 package dssc.roman;
 
 public class RomanNumeral {
+    private int decimal;
+
     public RomanNumeral(int decimal) {
+        this.decimal = decimal;
     }
 
     @Override
     public String toString() {
-        return "";
+        if (this.decimal == 0) {
+            return "";
+        } else {
+            return "I";
+        }
     }
 }
